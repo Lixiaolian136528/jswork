19c1a74c1760928883cdd4838ac57f07d3129a38
Lixiaolian136528 <1505754919@qq.com>
2019-12-08 02:16:05 +0800

    work14

--- work14-2/index2.js
+++ work14-2/index2.js
@@ -1,11 +1,11 @@
 var hours = new Date().getHours();
 var minutes = new Date().getMinutes();
-var f = 60-minutes;
+var f = 59 - minutes;
 var second = new Date().getSeconds();
-var m = 60-second;
+var m = 59 - second;
 
 let endseconds =new Date().getTime()+f*1000*60+m*1000;
-let d = h = m = s =0;
+let d = h = m = s = 0;
 let id = setInterval(seckill,1000);
 
 function seckill(){
@@ -14,9 +14,9 @@ function seckill(){
 
     if(remaining >0){
         d=parseInt(remaining / 86400);
-        h = parseInt((remaining/3600)%24);
-        m = parseInt((remaining/60)%60);
-        s = parseInt(remaining%60);
+        h = parseInt((remaining / 3600) % 24);
+        m = parseInt((remaining / 60) % 60);
+        s = parseInt(remaining % 60);
 
         d = d<10 ? '0' + d : d;
         h = h<10 ? '0' + h : h;
@@ -27,6 +27,7 @@ function seckill(){
         clearInterval(id);
         d= h =m = s ='00';
     }
+
     document.getElementById('d').innerHTML = d + '天'
     document.getElementById('h').innerHTML = h + '时'
     document.getElementById('m').innerHTML = m + '分'
